2bfe1097b56393a2d5b2cce4c740881de531e93f
Ivan Amoros prados <iamoros-@cbr7s5.42barcelona.com>
2023-10-06 19:05:17 +0200

    all correct in stack b

--- Push_swap/src/complex_sort.c
+++ Push_swap/src/complex_sort.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   complex_sort.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: ivanamoros <ivanamoros@student.42.fr>      +#+  +:+       +#+        */
+/*   By: iamoros- <iamoros-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/28 21:25:04 by iamoros-          #+#    #+#             */
-/*   Updated: 2023/10/05 14:51:59 by ivanamoros       ###   ########.fr       */
+/*   Updated: 2023/10/06 18:57:19 by iamoros-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,36 @@ int	hold_second_position(t_stack **stack, int to_chunk)
 	return (hold_second - 1);
 }
 
+int	first_a_smaller_first_b(t_stack *stack_a, t_stack *stack_b)
+{
+	t_stack	*tmp;
+	int		smaller_in_b;
+	int		first_b_smaller;
+
+	//sleep(1);
+	tmp = stack_a;
+	smaller_in_b = 0;
+	first_b_smaller = 0;
+	if (ft_lstsize_push_swap(stack_b) > 1)
+	{
+		while (tmp)
+		{
+			if (stack_a->value < tmp->value)
+			{
+				smaller_in_b = 1;
+				break ;
+			}
+			tmp = tmp->next;
+		}
+		//printf("STACK A: %i\nSTACK B: %i\n", stack_b->value, stack_b->value);
+		if (stack_a->value < stack_b->value)
+			first_b_smaller = 1;
+	}
+	if (smaller_in_b == 1 && first_b_smaller == 1)
+		return (1);
+	return (0);
+}
+
 void	sort_100(t_stack **stack_a, t_stack **stack_b)
 {
 	int	stack_size;
@@ -63,15 +93,18 @@ void	sort_100(t_stack **stack_a, t_stack **stack_b)
 	{
 		while (hold_first_position(stack_a, 100 / 5 * i) != -1)
 		{
-			//printf("\n\nHOLD FIRST: %i", hold_first_position(stack_a, 100 / 5 * i));
-			//printf("\nHOLD SECOND: %i\n\n", ft_lstsize_push_swap(*stack_a) - hold_second_position(stack_a, 100 / 5 * i));
-			//sleep(1);
-			if (hold_first_position(stack_a, 100 / 5 * i) >= ft_lstsize_push_swap(*stack_a) - hold_second_position(stack_a, 100 / 5 * i) )
+			if (hold_first_position(stack_a, 100 / 5 * i) >= ft_lstsize_push_swap(*stack_a) - hold_second_position(stack_a, 100 / 5 * i))
 				ra(stack_a);
 			else
 				rra(stack_a);
 			if (hold_first_position(stack_a, 100 / 5 * i) == 0)
+			{
 				pb(stack_a, stack_b);
+				//printf("NUMEROO; %i\n", (100 / 5 * i) - 10);
+				//printf("INDICE; %i\n", (*stack_b)->index);
+				if ((*stack_b)->index < (100 / 5 * i) - 10 && i != 1)
+					rb(stack_b);
+			}
 		}
 		i++;
 	}
